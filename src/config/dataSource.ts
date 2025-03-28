aafd56023d1759410a3c875c9a0d6a7dde37188a
Lucy Villogas <lvillogas.training@innevo.cl>
2025-03-28 12:57:54 -0500

    supa4

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -12,9 +12,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: process.env.SSL === "true"
-    ? { rejectUnauthorized: false } // SSL en producción
-    : undefined, // Sin SSL en desarrollo
+  ssl:{ rejectUnauthorized: false },
   entities: [User, Specialty, Doctor, Appointment],
   synchronize: false, 
   extra: {
